e4703b0c820f9a072ad3f80f81be00b6fbf6c87a
rafasor <Angelhunter8253@gmail.com>
2023-06-20 17:14:07 -0300

    Security

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,12 +5,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyBWJWzHwYgNAwSOZHy4gfk7caX-CMmbSpQ",
-            authDomain: "compostapp-f08a6.firebaseapp.com",
-            projectId: "compostapp-f08a6",
-            storageBucket: "compostapp-f08a6.appspot.com",
-            messagingSenderId: "436556068608",
-            appId: "1:436556068608:web:f41b828251195cc80a65e5"));
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: ""));
   } else {
     await Firebase.initializeApp();
   }
